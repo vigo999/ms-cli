63b8002d260bea128f01cf420d1ea2722b810918
vigo999 <zwiori1982@163.com>
2026-02-26 20:20:15 +0800

    rm common/local/docker concept.

--- executor/common/stream.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// StreamChunk is a chunk of stdout/stderr.
-type StreamChunk struct {
-	Source string
-	Data   string
-}
--- executor/docker/runner.go
+++
@@ -1,4 +0,0 @@
-package docker
-
-// Runner is a docker-backed execution adapter.
-type Runner struct{}
